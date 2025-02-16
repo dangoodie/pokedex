14f26255abeb1c8f7274b89ee8b7f35ec992d55e
dangoodie <dan.gooden.dev@gmail.com>
2025-02-16 21:21:40 +1100

    Repl built

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		scanner.Scan()
+		args := cleanInput(scanner.Text())
+		if len(args) != 0 {
+			fmt.Printf("Your command was: %s\n", args[0])
+		}
+	}
 }
 
 func cleanInput(text string) []string {
