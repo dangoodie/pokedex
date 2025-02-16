8c45ada0c3772dca91e17cae499f32bd8f8416ec
dangoodie <dan.gooden.dev@gmail.com>
2025-02-16 13:43:16 +1100

    Clean Input and Testing set up

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dangoodie/pokedex
+
+go 1.24.0
---
+++ repl_test.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"testing"
+	"strings"
+)
+
+// Test Clean Input Function
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "     hello  world     ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "foo    bar",
+			expected: []string{"foo", "bar"},
+		},
+		{
+			input:    "   singleword   ",
+			expected: []string{"singleword"},
+		},
+		{
+			input:    "multiple   spaces   between   words",
+			expected: []string{"multiple", "spaces", "between", "words"},
+		},
+		{
+			input:    "   leading and trailing spaces   ",
+			expected: []string{"leading", "and", "trailing", "spaces"},
+		},
+		{
+			input:    "",
+			expected: []string{},
+		},
+		{
+			input:    "   ",
+			expected: []string{},
+		},
+		{
+			input:    "CAPITALIZED WORDS SHOULD BE CONVERTED TO LOWERCASE",
+			expected: []string{"capitalized", "words", "should", "be", "converted", "to", "lowercase"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		// Check the length of the actual slice
+
+		if len(actual) != len(c.expected) {
+			t.Errorf("Slice length mismatched! Actual: %v Expected: %v", len(actual), len(c.expected))
+			t.Fail()
+		}
+
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+
+			if strings.Compare(word, expectedWord) != 0 {
+				t.Errorf("Words don't match! Word: %v Expected: %v", word, expectedWord)
+				t.Fail()
+			}
+		}
+	}
+}
