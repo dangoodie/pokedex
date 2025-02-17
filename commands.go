52e8e975f6f50d56cd8089663b7455e3ef26271a
dangoodie <dan.gooden.dev@gmail.com>
2025-02-17 12:38:21 +1100

    Contacting the API and Unmarshalling JSON now

--- commands.go
+++ commands.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
+)
+
+const (
+	apiUrl = "https://pokeapi.co/api/v2/"
 )
 
 type cliCommand struct {
@@ -13,9 +19,14 @@ type cliCommand struct {
 
 func getCommands() map[string]cliCommand {
 	return map[string]cliCommand{
+		"map": {
+			name:        "map",
+			description: "Use your map to find an area",
+			callback:    commandMap,
+		},
 		"help": {
 			name:        "help",
-			description: "Diaplay a help message",
+			description: "Display a help message",
 			callback:    commandHelp,
 		},
 		"exit": {
@@ -24,6 +35,47 @@ func getCommands() map[string]cliCommand {
 			callback:    commandExit,
 		},
 	}
+}
+
+type PokeMap struct {
+	Count    int     `json:"count"`
+	Next     *string `json:"next"`
+	Previous *string `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
+
+func commandMap() error {
+	// Make Get request
+	res, err := http.Get(apiUrl + "location-area/")
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	// Unmarshal the JSON
+	var pokeMap PokeMap
+	decoder := json.NewDecoder(res.Body)
+	err = decoder.Decode(&pokeMap)
+	if err != nil {
+		return err
+	}
+
+	// Print the JSON data for now
+	fmt.Printf("Count: %d\n", pokeMap.Count)
+	if pokeMap.Next != nil {
+		fmt.Printf("Next: %s\n", *pokeMap.Next)
+	}
+	if pokeMap.Previous != nil {
+		fmt.Printf("Previous: %s\n", *pokeMap.Previous)
+	}
+	for i, result := range pokeMap.Results {
+		fmt.Printf("%d: %s\n", i+1, result.Name)
+	}
+
+	return nil
 }
 
 func commandHelp() error {
